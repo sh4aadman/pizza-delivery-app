b8b069212424d6bbde12d1d87ac1355c86924d3c
sh4aadman <sh4adman@gmail.com>
2025-03-23 18:56:16 +0600

    completes custom order

--- src/pages/Pizzas/Pizzas.jsx
+++ src/pages/Pizzas/Pizzas.jsx
@@ -4,7 +4,7 @@ import Heading from "/src/shared/Heading/Heading";
 export default function Pizzas() {
   return (
     <>
-      <form className="md:w-5/6 mx-auto my-11 py-8 flex flex-col gap-12">
+      <form className="md:w-5/6 mx-auto my-11 py-8 flex flex-col items-start gap-12">
         <Heading content={"Custom Pizza"} />
         <Labels
           name={"base"}
@@ -40,6 +40,13 @@ export default function Pizzas() {
           name={"veggie"}
           options={["Bell Peppers", "Mushrooms", "Olives", "Onions", "Opt Out"]}
         />
+        <label className="font-light text-[#000000] text-2xl">
+          Enter quantity:
+          <input className="ml-4" type="number"></input>
+        </label>
+        <button className="font-light text-[#000000] text-2xl px-2 py-1 rounded-lg border-1" type="submit">
+          Place Order
+        </button>
       </form>
     </>
   );
--- src/shared/Labels/Labels.jsx
+++ src/shared/Labels/Labels.jsx
@@ -4,7 +4,7 @@ export default function Labels({name, options}) {
   return (
     <label className="font-light text-[#000000] text-2xl">
       Pick a {name}:
-      <select className="ml-4" name={name} id={name}>
+      <select className="ml-4 p-1" name={name} id={name}>
         <Options options={options} />
       </select>
     </label>
