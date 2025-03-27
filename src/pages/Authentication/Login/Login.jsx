07cc464b9b5d49370780b391544d77459a556710
sh4aadman <sh4adman@gmail.com>
2025-03-27 19:07:24 +0600

    integrates react form hook

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -1,18 +1,40 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router";
 
 export default function Login() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="bg-[#F5F5F5] font-inter font-light text-[#000000] bg-[url(/src/assets/backdrops/backdrop.png)] bg-no-repeat bg-right h-screen flex justify-center items-center">
-      <form className="border-1 rounded-xl p-10">
+      <form
+        className="border-1 rounded-xl p-10"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <h1 className="text-4xl">Login</h1>
         <p className="text-2xl my-10">Welcome back!</p>
         <label className="block my-5 text-2xl">
           Email :
-          <input name="email" id="email" type="text" />
+          <input
+            {...register("email")}
+            className="focus:outline-none focus:bg-transparent focus:border-b-1 focus:ml-2 ml-2 italic font-light"
+            name="email"
+            id="email"
+            type="text"
+          />
         </label>
         <label className="block my-5 text-2xl">
           Password :
-          <input name="password" id="password" type="password" />
+          <input
+            {...register("password")}
+            className="focus:outline-none focus:bg-transparent focus:border-b-1 focus:ml-2 ml-2 italic font-light"
+            name="password"
+            id="password"
+            type="password"
+          />
         </label>
         <button
           className="text-2xl mt-5 mb-2 px-2 py-1 rounded-lg border-1"
@@ -20,7 +42,12 @@ export default function Login() {
         >
           Login
         </button>
-        <p className="my-2">Create an account? <Link className="underline" to="/signup">Signup</Link></p>
+        <p className="my-2">
+          Create an account?{" "}
+          <Link className="underline" to="/signup">
+            Signup
+          </Link>
+        </p>
       </form>
     </div>
   );
